720e2b143cbffef3bdc5cb6dc453027659cfdbeb
Svenne77 <91331672+Svenne77@users.noreply.github.com>
2021-09-25 14:54:09 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,131 @@
+<?php
+
+/*
+Introduction:
+
+This configuration is pretty easy to edit. It's self explanatory as you can look at the web page while editing it to get a clear understanding.
+Thank you for purchasing and I wish you goodluck with the purpose to as why you are using this template.
+
+Message:
+This is last update for this template, unless it's to fix bugs.
+I will be releasing a brand new template with an Admin Panel within the next two weeks as of the 15/04/2021!
+Thank you again.
+*/
+
+
+
+
+
+
+
+
+// Meta Data
+$title = "Qrows Alt Shop";
+$desc = "Lorem ipsum...";
+$favicon = "";
+
+// Nav Section
+$Brand = "Qrow";
+$Navbar = [
+    [
+        "Name" => "Home",
+        "Href" => "index.html"
+    ],
+    [
+        "Name" => "Discord",
+        "Href" => "https://discord.gg/"
+    ],
+];
+
+// Header Info
+// Heading One - "Welcome to Qrows Alt Shop"
+$heading1 = "Welcome to Qrows Alt Shop";
+// Heading description - "Lorem ipsum..."
+$headingdesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu, venenatis mattis proin libero accumsan. Nulla nunc mattis nulla faucibus nisl, tellus, senectus mattis odio. Nulla viverra semper magnis.";
+// Logo - The image on the right
+$logo = "assets/img/Epic.png";
+
+
+// Section One
+$Title1 = "Features";
+$Desc1 = "Why purchase from our shop?";
+
+// Features
+$Features = [
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+    [
+        "Icon" => "assets/img/dollar-symbol 1.svg",
+        "Title" => "Affordable Prices"
+    ],
+];
+// Section Two
+$Title2 = "Products";
+$Desc2 = "What services do we sell?";
+
+// Categories
+$Categories = [
+    [
+        "Name" => "One",
+    ],
+    [
+        "Name" => "Two",
+    ]
+];
+
+// Products
+$Products = [
+    [
+        "Category" => "One",
+        "ProductName" => "Example",
+        "ProductImage" => "assets/img/Epic.png",
+        "Embed" => "604514a19c3bc",
+        "ProductDesc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+    [
+        "Category" => "Two",
+        "ProductName" => "Examffffple",
+        "ProductImage" => "assets/img/Epic.png",
+        "Embed" => "604514a19c3bc",
+        "ProductDesc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+    [
+        "Category" => "Two",
+        "ProductName" => "Examffffple",
+        "ProductImage" => "assets/img/Epic.png",
+        "Embed" => "604514a19c3bc",
+        "ProductDesc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+    [
+        "Category" => "Two",
+        "ProductName" => "Examffffple",
+        "ProductImage" => "assets/img/Epic.png",
+        "Embed" => "604514a19c3bc",
+        "ProductDesc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+    [
+        "Category" => "Two",
+        "ProductName" => "Examffffple",
+        "ProductImage" => "assets/img/Epic.png",
+        "Embed" => "604514a19c3bc",
+        "ProductDesc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    ],
+];
